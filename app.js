fedcdace2c4764c5e6d2679d03f0798a0a23f072
Chris <plasmaticdarkness@gmail.com>
2023-06-26 00:16:33 -0400

    Working html and css requests for main page

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 const { error } = require('console')
 const fs = require('fs')
-const http = require('http')
+const http = require('http');
+const { url } = require('inspector');
 const port = 3000;
 
-const server = http.createServer(function(req, res){
+//RequestMap holds request keys to matching functions 
+const RequestMap = new Map();
+RequestMap.set('/', LoadMainPage)
+RequestMap.set('/style.css', LoadMainPageStyle)
+
+function LoadMainPage(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html'})
-    fs.readFile('client/index.html', function(error, data){
+    fs.readFile('MainPage/index.html', function(error, data){
         if(error){
-            res.writeHead(404)
-            res.write('Error: File Not Found')
+            error404(res)
         } else {
             res.write(data)
         }
         res.end()
     })
+}
+
+function LoadMainPageStyle(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/css'})
+    fs.readFile('MainPage/style.css', function(error, data){
+        if(error){
+            error404(res)
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+}
+
+const server = http.createServer(function(req, res){
+    var address = new URL(req.url, `http://${req.headers.host}`);
+    //console.log(address.pathname)
+    if(RequestMap.get(address.pathname) != undefined){
+        RequestMap.get(address.pathname)(req, res)
+    } else {
+        if(error){
+            error404(res)
+        }
+        res.end()
+    }
 });
+
+//Print 404 error and send 404 header
+function error404(response){
+    response.writeHead(404)
+    response.write('Error: File Not Found')
+}
 
 server.listen(port, function(error){
     if(error){
