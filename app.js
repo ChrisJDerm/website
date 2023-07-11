edc73bf999a3453cbbbec28df445900f8dbcd2fc
Chris <plasmaticdarkness@gmail.com>
2023-07-11 12:14:15 -0400

    regex stuff

--- app.js
+++ app.js
@@ -1,63 +1,89 @@
 const { error } = require('console')
-const fs = require('fs')
+const express = require('express')
 const http = require('http');
 const { url } = require('inspector');
-const port = 3000;
+const app = express()
+const port = 80;
+
 
 //RequestMap holds request keys to matching functions 
 const RequestMap = new Map();
-RequestMap.set('/', LoadMainPage)
-RequestMap.set('/style.css', LoadMainPageStyle)
-RequestMap.set('/MainPage.js', LoadMainPageJS)
+RequestMap.set('/', 'MainPage/index.html')
+RequestMap.set('/style.css', 'MainPage/style.css')
+//RequestMap.set('/MainPage.js', LoadMainPageJS)
 
-function LoadMainPage(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/html'})
-    fs.readFile('MainPage/index.html', function(error, data){
-        if(error){
-            error404(res)
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-}
+// app.get('/', (req, res) => {
+//     res.sendFile('MainPage/index.html', {"root":"C:/Website"})
+// })
 
-function LoadMainPageStyle(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/css'})
-    fs.readFile('MainPage/style.css', function(error, data){
-        if(error){
-            error404(res)
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-}
+app.get(/./, (req, res) => {
+    res.sendFile(RequestMap.get(req.url), {"root":"C:/Website"})
+})
 
-function LoadMainPageJS(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/javascript'})
-    fs.readFile('MainPage/MainPage.js', function(error, data){
-        if(error){
-            error404(res)
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-}
+// app.get(/(\.html)/, (req, res) => {
+//     res.sendFile(RequestMap.get(req.url), {"root":"C:/Website"})
+// })
 
-const server = http.createServer(function(req, res){
-    var address = new URL(req.url, `http://${req.headers.host}`);
-    //console.log(address.pathname)
-    if(RequestMap.get(address.pathname) != undefined){
-        RequestMap.get(address.pathname)(req, res)
-    } else {
-        if(error){
-            error404(res)
-        }
-        res.end()
-    }
-});
+// app.get(/(\.css)/, (req, res) => {
+//     res.sendFile(RequestMap.get(req.url), {"root":"C:/Website", "headers":"Content-Type: text/css"})
+// })
+
+// app.get(/(\.js)/, (req, res) => {
+//     res.send
+// })
+
+app.listen(port, () => {
+    console.log('Listening on port ' + String(port))
+})
+
+// function LoadMainPage(req, res){
+//     res.writeHead(200, { 'Content-Type': 'text/html'})
+//     fs.readFile('MainPage/index.html', function(error, data){
+//         if(error){
+//             error404(res)
+//         } else {
+//             res.write(data)
+//         }
+//         res.end()
+//     })
+// }
+
+// function LoadMainPageStyle(req, res){
+//     res.writeHead(200, { 'Content-Type': 'text/css'})
+//     fs.readFile('MainPage/style.css', function(error, data){
+//         if(error){
+//             error404(res)
+//         } else {
+//             res.write(data)
+//         }
+//         res.end()
+//     })
+// }
+
+// function LoadMainPageJS(req, res){
+//     res.writeHead(200, { 'Content-Type': 'text/javascript'})
+//     fs.readFile('MainPage/MainPage.js', function(error, data){
+//         if(error){
+//             error404(res)
+//         } else {
+//             res.write(data)
+//         }
+//         res.end()
+//     })
+// }
+
+// const server = http.createServer(function(req, res){
+//     var address = new URL(req.url, `http://${req.headers.host}`);
+//     //console.log(address.pathname)
+//     if(RequestMap.get(address.pathname) != undefined){
+//         RequestMap.get(address.pathname)(req, res)
+//     } else {
+//         if(error){
+//             error404(res)
+//         }
+//         res.end()
+//     }
+// });
 
 //Print 404 error and send 404 header
 function error404(response){
@@ -65,10 +91,10 @@ function error404(response){
     response.write('Error: File Not Found')
 }
 
-server.listen(port, function(error){
-    if(error){
-        console.log('Something went wrong: ', error)
-    } else {
-        console.log('Server listening on port ' + port)
-    }
-});
+// server.listen(port, function(error){
+//     if(error){
+//         console.log('Something went wrong: ', error)
+//     } else {
+//         console.log('Server listening on port ' + port)
+//     }
+// });
